5b1e98abbc19aa6fe995d604532195800b577696
Christopher Black <chris@Christopher-Blacks-MacBook.local>
2012-11-28 20:01:51 -0600

    Added button for centering on current location. Improved view resizing logic. Added boiler plate test case class and connected up to application.

---
+++ NearbyTests/NearbyTests.h
@@ -0,0 +1,13 @@
+//
+//  NearbyTests.h
+//  NearbyTests
+//
+//  Created by Christopher Black on 11/28/12.
+//  Copyright (c) 2012 Blacktop Interactive, LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NearbyTests : SenTestCase
+
+@end
---
+++ NearbyTests/NearbyTests.m
@@ -0,0 +1,32 @@
+//
+//  NearbyTests.m
+//  NearbyTests
+//
+//  Created by Christopher Black on 11/28/12.
+//  Copyright (c) 2012 Blacktop Interactive, LLC. All rights reserved.
+//
+
+#import "NearbyTests.h"
+
+@implementation NearbyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NearbyTests");
+}
+
+@end
